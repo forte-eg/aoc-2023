c814721c60d1016f2138ac55ea711afba6a1faa6
Erol Gorancic <erol.gorancic@fortedigital.no>
2023-12-01 14:22:52 +0100

    implement day1 part1

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+
+fn main() -> Result<(), ()> {
+    let mut pwd = env::current_dir()
+        .map_err(|err| eprintln!("{err}"))?;
+    pwd.push("input");
+
+    let contents = fs::read_to_string(pwd.as_path())
+        .map_err(|err| eprintln!("{err}"))?;
+
+    let mut codes: Vec<u32> = Vec::new();
+    let mut digits: Vec<u32> = Vec::new();
+    for line in contents.split("\n") {
+        digits.clear();
+        for char in line.chars() {
+            char.to_digit(10).map(|digit| digits.push(digit));
+        }
+        let mut code: u32 = 0;
+        digits.first().map(|digit| code = 10 * *digit);
+        digits.last().map(|digit| code += *digit);
+        codes.push(code);
+        println!("{:?} -> {code}", digits);
+    }
+    let sum: u32 = codes.iter().sum();
+    println!("{sum}");
+    Ok(())
 }
