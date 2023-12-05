4cf88ea5d98e2d9dec53d45e9bbebb72a8312780
Erol Gorancic <erol.gorancic@fortedigital.com>
2023-12-05 23:06:39 +0100

    day 5 part 2

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -1,3 +1,4 @@
+// use std::collections::HashMap;
 use read_input::read_input;
 
 struct QuickMapEntry {
@@ -7,15 +8,8 @@ struct QuickMapEntry {
 }
 
 struct QuickMap {
-    entries: Vec<QuickMapEntry>
+    entries: Vec<QuickMapEntry>,
 }
-
-// 98 -> 50
-// 99 -> 51
-
-//  in(x) = x € [src..src+count]
-// get(x) = (dst - src) + x  | in(x) == true
-// get(x) = x                | in(x) == false
 
 impl QuickMapEntry {
     fn parse(numbers: &str) -> QuickMapEntry {
@@ -29,46 +23,83 @@ impl QuickMapEntry {
 
 impl QuickMap {
     fn parse(block: &str) -> QuickMap {
-        let entries = block.lines().skip(1)
+        let mut entries: Vec<_> = block.lines().skip(1)
             .map(|line| QuickMapEntry::parse(line))
             .collect();
+        entries.sort_by(|a, b| a.lower.cmp(&b.lower));
         QuickMap { entries }
     }
 
-    // TODO: sorted + binary search?
+    // binary search the entries
     fn get(&self, key: i64) -> i64 {
-        self.entries.iter()
-            .find(|entry| key >= entry.lower && key < entry.upper)
-            .map(|entry| entry.diff + key)
-            .unwrap_or(key)
+        let entries = &self.entries;
+        let mut l = 0usize;
+        let mut r = entries.len() - 1;
+
+        while l <= r {
+            let m = l + (r - l) / 2;
+            let entry = &entries[m];
+            if key >= entry.lower && key < entry.upper {
+                return entry.diff + key;
+            }
+            if key >= entry.upper {
+                l = m + 1;
+            } else {
+                if m == 0 {break;}
+                r = m - 1;
+            }
+        }
+        key
     }
 }
+
+// struct CachedSearch {
+//     maps: Vec<QuickMap>,
+//     cache: HashMap<(i64, QuickMap), i64>,
+// }
+//
+// impl CachedSearch {
+//     fn new(maps: Vec<QuickMap>) -> CachedSearch {
+//         CachedSearch { maps, cache: HashMap::new() }
+//     }
+//
+//     fn lookup(&self, key: i64) -> i64 {
+//         let mut val = key;
+//         for map in &self.maps {
+//             val = map.get(val);
+//             if self.cache[(val, map)]
+//         }
+//         val
+//     }
+// }
 
 fn main() -> Result<(), ()> {
     let contents = read_input()?;
     let result = run(&contents)?;
-    println!("silver: {result}");
+    println!("gold: {result}");
     Ok(())
 }
 
 fn run(input: &str) -> Result<i64, ()> {
     let lines: Vec<_> = input.split("\n\n").collect();
-
     let seeds: Vec<_> = lines[0].split_whitespace().skip(1).map(|s| s.parse::<i64>().unwrap()).collect();
-    let maps: Vec<_> = lines[1..].iter()
-        .map(|s| QuickMap::parse(s))
-        .collect();
+    let maps: Vec<_> = lines[1..].iter().map(|s| QuickMap::parse(s)).collect();
 
-    let mut locations = Vec::new();
-    for seed in seeds {
-        let mut location = seed;
-        for map in &maps {
-            location = map.get(location);
+    let mut min = i64::MAX;
+    for seed_chunk in seeds.chunks(2) {
+        let [seed_0, count] = seed_chunk else { panic!() };
+        for i in 0..*count {
+            let mut location = seed_0 + i;
+            for map in &maps {
+                location = map.get(location);
+            }
+            if location < min {
+                min = location;
+            }
         }
-        locations.push(location);
     }
 
-    Ok(*locations.iter().min().unwrap())
+    Ok(min)
 }
 
 #[cfg(test)]
@@ -77,9 +108,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn example_p1() {
+    fn example_p2() {
         let contents = read_test_input().unwrap();
         let result = run(&contents).unwrap();
-        assert_eq!(35, result);
+        assert_eq!(46, result);
     }
 }
