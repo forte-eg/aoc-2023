9368ef6bdc874b43fb7ea36f08976b07dcf7e2ff
Erol Gorancic <erol.gorancic@fortedigital.com>
2023-12-06 20:57:06 +0100

    day 6 part 2

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -2,18 +2,31 @@ use read_input::read_input;
 
 fn main() -> Result<(), ()> {
     let contents = read_input()?;
-    let result: i32 = run(&contents).iter().product();
-    println!("{result}");
+    let result: i64 = part1(&contents).iter().product();
+    println!("silver: {result}");
+    let result = part2(&contents)[0];
+    println!("gold: {result}");
     Ok(())
 }
 
-fn run(input: &str) -> Vec<i32> {
-    let numbers: Vec<Vec<i32>> = input.lines()
-        .map(|l| l.split_whitespace().skip(1).map(|s| s.parse::<i32>().unwrap()).collect())
+fn part1(input: &str) -> Vec<i64> {
+    let numbers: Vec<Vec<i64>> = input.lines()
+        .map(|l| l.split_whitespace().skip(1).map(|s| s.parse::<i64>().unwrap()).collect())
         .collect();
+    run(&numbers[0], &numbers[1])
+}
 
-    let mut wins: Vec<i32> = Vec::new();
-    for (time, dist) in numbers[0].iter().zip(numbers[1].iter()) {
+fn part2(input: &str) -> Vec<i64> {
+    let numbers: Vec<i64> = input.lines()
+        .map(|l| l.split_whitespace().skip(1).collect::<String>())
+        .map(|s| s.parse::<i64>().unwrap())
+        .collect();
+    run(&[numbers[0]], &[numbers[1]])
+}
+
+fn run(times: &[i64], dists: &[i64]) -> Vec<i64> {
+    let mut wins: Vec<i64> = Vec::new();
+    for (time, dist) in times.iter().zip(dists.iter()) {
         let mut w = 0;
         for wait in 0..*time {
             let t = time - wait;
@@ -36,7 +49,7 @@ mod tests {
     #[test]
     fn example_p1() {
         let contents = read_test_input().unwrap();
-        let wins = run(&contents);
+        let wins = part1(&contents);
         let mut x = wins.iter();
         assert_eq!(Some(4), x.next().copied());
         assert_eq!(Some(8), x.next().copied());
