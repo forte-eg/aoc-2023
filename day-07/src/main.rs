53f668084148f60ddcd2e558b0006dd61e2138e8
Erol Gorancic <erol.gorancic@fortedigital.com>
2023-12-10 20:44:55 +0100

    day 7 part 2 brute foooorce

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp;
 use std::cmp::Ordering;
 use std::collections::HashMap;
 use read_input::read_input;
@@ -13,7 +14,7 @@ struct Hand<'a> {
 impl<'a> Hand<'a> {
     fn parse(line: &'a str) -> Hand {
         let [cards, bid] = line.split_whitespace().collect::<Vec<_>>()[..] else { panic!() };
-        Hand { cards , bid: bid.parse().unwrap(), strength: Hand::hand_strength(cards), label_strengths: Hand::label_strength(cards) }
+        Hand { cards , bid: bid.parse().unwrap(), strength: Hand::hand_strength(cards), label_strengths: Hand::label_strengths(cards) }
     }
 
     fn hand_strength(cards: &str) -> u32 {
@@ -22,40 +23,66 @@ impl<'a> Hand<'a> {
             let old = map.get(&card).unwrap_or(&0);
             map.insert(card, *old + 1);
         }
-        let mut groups = map.values().map(|i| *i).collect::<Vec<_>>();
+
+        match map.get(&'J').copied() {
+            Some(5) => { map.remove(&'J'); map.insert('A', 5); },
+            Some(_) => { return Hand::brute_force(&mut map); },
+            _ => {},
+        }
+
+        let mut values = map.values().map(|a| *a).collect();
+        return Hand::strength(&mut values);
+    }
+
+    fn brute_force(map: &mut HashMap<char, u32>) -> u32 {
+        let mut strength = 0;
+        let count = map.remove(&'J').unwrap();
+        for key in map.keys() {
+            let mut groups = map.iter().map(|(k, v)| if k == key {*v + count} else {*v}).collect();
+            strength = cmp::max(Hand::strength(&mut groups), strength);
+        }
+        return strength;
+    }
+
+    fn strength(groups: &mut Vec<u32>) -> u32 {
         groups.sort_unstable_by(|a,b| b.cmp(a));
         return match groups[..] {
             [5] => 7,
             [4,..] => 6,
-            [3,2,..] => 5,
+            [3,2] => 5,
             [3,..] => 4,
-            [2, 2,..] => 3,
+            [2,2,..] => 3,
             [2,..] => 2,
-            _ => 1
+            [1,1,1,1,1] => 1,
+            _ => {println!("ERROR: {groups:?}"); 0}
         }
     }
 
-    fn label_strength(cards: &str) -> [u32; 5] {
+    fn label_strengths(cards: &str) -> [u32; 5] {
         let mut strengths = [1u32; 5];
         let chars = cards.chars().collect::<Vec<_>>();
         for i in 0..5 {
-            strengths[i] = match chars[..][i] {
-                x if x.is_digit(10) => x.to_digit(10).unwrap(),
-                'T' => 10,
-                'J' => 11,
-                'Q' => 12,
-                'K' => 13,
-                'A' => 14,
-                _ => panic!()
-            };
+            strengths[i] = Hand::label_strength(&chars[i]);
         }
         return strengths;
+    }
+
+    fn label_strength(c: &char) -> u32 {
+        return match c {
+            x if x.is_digit(10) => x.to_digit(10).unwrap(),
+            'J' => 1,
+            'T' => 10,
+            'Q' => 12,
+            'K' => 13,
+            'A' => 14,
+            _ => panic!()
+        };
     }
 }
 
 impl<'a> PartialEq<Self> for Hand<'a> {
     fn eq(&self, other: &Self) -> bool {
-        self.cards.eq(other.cards)
+        self.label_strengths.eq(&other.label_strengths)
     }
 }
 
@@ -73,13 +100,16 @@ impl<'a> PartialOrd for Hand<'a> {
 fn main() -> Result<(), ()> {
     let contents = read_input()?;
     let total_winings = run(&contents);
-    println!("silver: {total_winings}");
+    println!("gold: {total_winings}");
     Ok(())
 }
 
 fn run(input: &str) -> u64 {
     let mut hands: Vec<_> = input.lines().map(|line| Hand::parse(line)).collect();
     hands.sort_unstable();
+    for hand in &hands {
+        println!("{hand:?}");
+    }
     hands.iter().enumerate().fold(0, |acc, (i, hand)| acc + ((i as u64 + 1) * hand.bid))
 }
 
@@ -88,10 +118,32 @@ mod tests {
     use super::*;
     use read_input::read_test_input;
 
+    // #[test]
+    // fn example_p1() {
+    //     let contents = read_test_input().unwrap();
+    //     let total = run(&contents);
+    //     assert_eq!(6440, total);
+    // }
+
     #[test]
-    fn example_p1() {
+    fn example_p2() {
         let contents = read_test_input().unwrap();
         let total = run(&contents);
-        assert_eq!(6440, total);
+        assert_eq!(5905, total);
+    }
+
+    #[test]
+    fn edge_1() {
+        let input = "JJJJJ 123";
+        let hand = Hand::parse(input);
+        assert_eq!([1,1,1,1,1], hand.label_strengths);
+        assert_eq!(7, hand.strength);
+    }
+
+    #[test]
+    fn edge_2() {
+        let input = "T55J5 684";
+        let hand = Hand::parse(input);
+        assert_eq!(6, hand.strength);
     }
 }
