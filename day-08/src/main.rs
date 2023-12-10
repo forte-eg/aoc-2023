c6ca0a728209990382de31f1d8851a6fde5983a0
Erol Gorancic <erol.gorancic@fortedigital.com>
2023-12-11 00:10:07 +0100

    day 8 wip

--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
 
 fn run(instructions: &[u8], map: &str) -> u64 {
     let mut lut: [(usize, usize); ARR_SZ] = [(0, 0); ARR_SZ];
-    populate(map, &mut lut);
+    let starting_nodes = populate(map, &mut lut);
 
     let mut steps = 0;
     let mut location = hash(&[b'A', b'A', b'A']);
@@ -29,14 +29,20 @@ fn run(instructions: &[u8], map: &str) -> u64 {
     return steps as u64;
 }
 
-fn populate(data: &str, lut: &mut [(usize, usize); ARR_SZ]) {
+fn populate(data: &str, lut: &mut [(usize, usize); ARR_SZ]) -> Vec<usize> {
+    let mut starting_nodes = Vec::new();
     let lines = data.split("\n").map(|s| s.as_bytes()).collect::<Vec<_>>();
     for line in lines {
         let k = &line[0..=2];
         let l = &line[7..=9];
         let r = &line[0xC..=0xE];
-        lut[hash(k)] = (hash(l), hash(r));
+        let key_hash = hash(k);
+        lut[key_hash] = (hash(l), hash(r));
+        if k[2] == b'A' {
+            starting_nodes.push(key_hash);
+        }
     }
+    return starting_nodes;
 }
 
 #[inline(always)]
